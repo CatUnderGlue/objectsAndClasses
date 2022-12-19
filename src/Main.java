134fbdd9a249cb0b4680d7a0cb9bca74b70bde0b
CatUnderGlue <warmonds1996@gmail.com>
2022-12-19 17:12:16 +0500

    Исправил замечания

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        Author Francis = new Author("Френсис", "Фицджеральд");
-        Author Alex = new Author("Александр", "Пушкин");
-        Book firstBook = new Book("Загадочная история Бенджамина Баттона", Francis, 1922);
-        Book secondBook = new Book("Капитанская дочка", Alex, 1936);
+        Author francis = new Author("Френсис", "Фицджеральд");
+        Author alex = new Author("Александр", "Пушкин");
+        Book firstBook = new Book("Загадочная история Бенджамина Баттона", francis, 1922);
+        Book secondBook = new Book("Капитанская дочка", alex, 1936);
         secondBook.setYearOfPublication(1937);
 
         System.out.printf("Автор книги %s, %d года выпуска - %s %s%n", firstBook.getTitle(), firstBook.getYearOfPublication(), firstBook.getAuthor().getFirstName(), firstBook.getAuthor().getLastName());
